1392cffc981e6f8b7cc38d02e8a8845418546271
BrigitaGrybaiteBartke <brigita.grybaite@gmail.com>
2022-09-11 11:42:33 +0300

    create upload logic

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ session_start();
 $message = '';
 $greeting = '';
 
-//login
+//login logic
 if ($_SERVER["REQUEST_METHOD"] == 'POST') {
     if (isset($_POST['submit']) and !empty($_POST['username'] and !empty($_POST['password']))) {
         $username = filter_var($_POST['username'], FILTER_SANITIZE_SPECIAL_CHARS);
@@ -32,12 +32,12 @@ if (isset($_GET['action']) == 'logout') {
     session_destroy();
 };
 
-// create new folder
-if(isset($_POST['newFolder'])) {
-    if(!empty($_POST['createNewFolder'])) {    
+// create new folder logic
+if (isset($_POST['newFolder'])) {
+    if (!empty($_POST['createNewFolder'])) {
         $newFolderName = $_POST['createNewFolder'];
         $dirCreate = './' . $newFolderName;
-        if(!is_dir($dirCreate)) {
+        if (!is_dir($dirCreate)) {
             mkdir($dirCreate, 0777, true);
             $dirMessage =  '<p style="color: blue">A new file created!</p>';
         } else {
@@ -46,12 +46,44 @@ if(isset($_POST['newFolder'])) {
     }
 }
 
+// upload folder logic
+if (isset($_POST['upload'])) {
+    $allowed_ext = ['png', 'jpg', 'jpeg', 'gif', 'svg'];
+    var_dump($allowed_ext);
+    if (!empty($_FILES['upload']['name'])) {
+        $file_name = $_FILES['upload']['name'];
+        $file_size = $_FILES['upload']['size'];
+        $file_tmp = $_FILES['upload']['tmp_name'];
+        $target_dir = "./${file_name}";
+        var_dump($target_dir);
+
+        $file_ext = explode('.', $file_name);
+        $file_ext = strtolower(end($file_ext));
+
+        if (in_array($file_ext, $allowed_ext)) {
+            if ($file_size <= 1000000) {
+                $move = move_uploaded_file($file_tmp, $target_dir);
+                var_dump($move);
+                $message = '<p style="color: green">File uploaded</p>';
+            } else {
+                $message = '<p style="color: red">File is too large</p>';
+            }
+        } else {
+            $message = '<p style="color: red">Invalid file type</p>';
+        }
+    } else {
+        $message = '<p style="color: red">Please choose a file</p>';
+    }
+}
+
+
 ?>
 
 
 <?php
 // testing
-
+// echo '<pre>';
+// var_dump($_FILES['upload'])
 
 
 ?>
@@ -71,6 +103,23 @@ if(isset($_POST['newFolder'])) {
     <style>
         .width {
             width: 350px;
+        }
+
+        table {
+            font-family: arial, sans-serif;
+            border-collapse: collapse;
+            width: 100%;
+        }
+
+        td,
+        th {
+            border: 3px solid #dddddd;
+            text-align: left;
+            padding: 8px;
+        }
+
+        tr:nth-child(even) {
+            background-color: #a59b9b;
         }
     </style>
 
@@ -117,33 +166,58 @@ if(isset($_POST['newFolder'])) {
         </div>
 
         <!-- create new folder -->
-            <div>
-                <div class="d-flex flex-column align-items-start width">
-                    <div class="text-center mt-5 mb-1">
-                        <h5>Create new folder</h5>
-                    </div>
-                    <div class="form-control">
-                        <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
-                            <div class="d-flex flex-column">
-                                <div>
-                                    <label for="newFolder" class="form-label">Folder name: </label>
-                                    <input type="text" name="createNewFolder" placeholder="Create new folder" class="form-control">
-                                </div>
-                                <div class="mt-2 align-self-end">
-                                    <input type="submit" name="newFolder" value="Create new folder" class="btn btn-primary">
-                                </div>
-
+        <div>
+            <div class="d-flex flex-column align-items-start width">
+                <div class="text-center mt-5 mb-1">
+                    <h5>Create new folder</h5>
+                </div>
+                <div class="form-control">
+                    <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
+                        <div class="d-flex flex-column">
+                            <div>
+                                <label for="newFolder" class="form-label">Folder name: </label>
+                                <input type="text" name="createNewFolder" placeholder="Create new folder" class="form-control">
                             </div>
-                        </form>
-                    </div>
-                    <div class="mt-3">
-                        <p><?php echo $dirMessage ?? null ?></p>
-                    </div>
+                            <div class="mt-2 align-self-end">
+                                <input type="submit" name="newFolder" value="Create new folder" class="btn btn-primary">
+                            </div>
+
+                        </div>
+                    </form>
+                </div>
+                <div class="mt-1">
+                    <p><?php echo $dirMessage ?? null ?></p>
                 </div>
             </div>
+        </div>
+
+        <!-- upload folder -->
+        <div class="form-control">
+            <form action="<?php echo $_SERVER["PHP_SELF"]; ?>" method="POST" enctype="multipart/form-data">
+                <div class="d-flex flex-column">
+                    <div>
+                        <label for="upload">Select file to upload</label>
+                        <input type="file" name="upload">
+                    </div>
+                    <div class="mt-2 align-self-end">
+                        <input type="submit" name="upload" value="Upload file">
+                    </div>
+                </div>
+            </form>
+            <div class="mt-3">
+                <?php echo $uplMessage ?? null ?>
+            </div>
+            <div>
+                <span>Image name: <?php echo $_FILES['upload']['name'] ?? 'No image selected!' ?></span><br>
+                <span>Image type: <?php echo $_FILES['upload']['type'] ?? 'No image selected!' ?></span><br>
+                <span>Image size: <?php echo $_FILES['upload']['size'] ?? 'No image selected!' ?></span><br>
+            </div>
+        </div>
 
 
+       
 
+    
 
     </div>
 
