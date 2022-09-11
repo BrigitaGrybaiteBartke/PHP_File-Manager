4856905209ed14e6ca5e762319e1467d1e153c32
BrigitaGrybaiteBartke <brigita.grybaite@gmail.com>
2022-09-11 18:45:19 +0300

    modify download section

--- download.php
+++ download.php
@@ -1,27 +1,25 @@
 <?php
 echo '<pre>';
-// var_dump($_GET['link']);
 
 if (isset($_GET['path'])) {
     $file = ('./' . $_GET['path']);
     $fileToDownloadEscaped = str_replace("&nbsp;", " ", htmlentities($file, 0, 'utf-8'));
 
+    if (file_exists($file)) {
 
-    if(file_exists($file)) {
-        
-    
         header('Content-Description: File Transfer');
         header('Content-Type: application/octet-stream');
-        header('Content-Disposition: attachment; filename="'.basename($file).'"');
+        header('Content-Disposition: attachment; filename="' . basename($file) . '"');
         header('Content-Transfer-Encoding: binary');
         header('Expires: 0');
         header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
         header('Pragma: public');
         header('Content-Length: ' . filesize($fileToDownloadEscaped));
-        
+
         ob_clean();
         flush();
         readfile($fileToDownloadEscaped);
+        
         exit;
-    }   
+    }
 }
